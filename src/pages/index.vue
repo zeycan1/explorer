617e77fc5441fbf625619d69ec65fed4a166f394
zeycan1 <canangurer@gmail.com>
2026-03-01 00:23:12 +0300

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,8 +8,10 @@ import {
 import ChainSummary from '@/components/ChainSummary.vue';
 import { computed, ref } from 'vue';
 import { useBlockchain } from '@/stores';
+
 const dashboard = useDashboard();
 const keywords = ref('');
+
 const chains = computed(() => {
   if (keywords.value) {
     const lowercaseKeywords = keywords.value.toLowerCase();
@@ -22,6 +24,7 @@ const chains = computed(() => {
     return Object.values(dashboard.chains);
   }
 });
+
 const featured = computed(() => {
   const names = [
     'cosmos',
@@ -37,13 +40,13 @@ const featured = computed(() => {
     .filter((x) => names.includes(x.chainName))
     .sort((a, b) => names.indexOf(a.chainName) - names.indexOf(b.chainName));
 });
+
 const chainStore = useBlockchain();
 </script>
+
 <template>
   <div class="">
-    <div
-      class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2"
-    >
+    <div class="flex md:!flex-row flex-col items-center justify-center mb-6 mt-14 gap-2">
       <div class="flex items-center gap-4 justify-center">
         <img src="/zeyca-logo.svg" alt="ZeycaNode" class="w-16 h-16 md:w-24 md:h-24" />
         <h1 class="text-black dark:text-white text-3xl md:!text-6xl font-bold">
@@ -51,52 +54,33 @@ const chainStore = useBlockchain();
         </h1>
       </div>
     </div>
-    <div
-      v-if="dashboard.status !== LoadingStatus.Loaded"
-      class="flex justify-center"
-    >
+
+    <div v-if="dashboard.status !== LoadingStatus.Loaded" class="flex justify-center">
       <progress class="progress progress-info w-80 h-1"></progress>
     </div>
-    <div
-      v-if="featured.length > 0"
-      class="text-center text-base mt-6 text-primary"
-    >
-      <h2 class="mb-6"> We monitor your networks for you :) </h2>
+
+    <div v-if="featured.length > 0" class="text-center text-base mt-6 text-primary">
+      <h2 class="mb-6">We monitor your networks for you :)</h2>
     </div>
-    <div
-      v-if="featured.length > 0"
-      class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
-    >
-      <ChainSummary
-        v-for="(chain, index) in featured"
-        :key="index"
-        :name="chain.chainName"
-      />
+
+    <div v-if="featured.length > 0" class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5">
+      <ChainSummary v-for="(chain, index) in featured" :key="index" :name="chain.chainName" />
     </div>
-    <div
-      class="flex items-center rounded-lg bg-base-100 border border-gray-200 dark:border-gray-700 mt-10"
-    >
+
+    <div class="flex items-center rounded-lg bg-base-100 border border-gray-200 dark:border-gray-700 mt-10">
       <Icon icon="mdi:magnify" class="text-2xl text-gray-400 ml-3" />
-      <input
-        :placeholder="$t('pages.search_placeholder')"
-        class="px-4 h-10 bg-transparent flex-1 outline-none text-base"
-        v-model="keywords"
-      />
+      <input :placeholder="$t('pages.search_placeholder')" class="px-4 h-10 bg-transparent flex-1 outline-none text-base" v-model="keywords" />
       <div class="px-4 text-base hidden md:!block">
         {{ chains.length }}/{{ dashboard.length }}
       </div>
     </div>
-    <div
-      class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5"
-    >
-      <ChainSummary
-        v-for="(chain, index) in chains"
-        :key="index"
-        :name="chain.chainName"
-      />
+
+    <div class="grid grid-cols-1 gap-4 mt-6 md:!grid-cols-3 lg:!grid-cols-4 2xl:!grid-cols-5">
+      <ChainSummary v-for="(chain, index) in chains" :key="index" :name="chain.chainName" />
     </div>
   </div>
 </template>
+
 <style scoped>
 .logo path {
   fill: #0f1419;
